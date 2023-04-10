49d74b9a6ca15d2253783dadb75c1eac3324b295
0x <34449718+z-btc@users.noreply.github.com>
2023-04-10 01:57:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ignoreBuildErrors: true,
   env: {
     // defaults to TRUE, unless API Keys are set at build time; this flag is used by the UI
     REQUIRE_USER_API_KEYS: !process.env.OPENAI_API_KEY,
